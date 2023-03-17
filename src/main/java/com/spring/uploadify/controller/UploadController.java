846b57560ca65f42f9d9b3168265b7f1f3e6a3f0
Rafhi Arizkia <doyokbsk@gmail.com>
2023-03-17 22:59:24 +0700

    feature(Spring Boot upload file dengan thymeleaf):

---
+++ src/main/java/com/spring/uploadify/controller/UploadController.java
@@ -0,0 +1,46 @@
+package com.spring.uploadify.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Controller
+public class UploadController {
+
+    private static final String UPLOADED_PATH = "D:\\SEM4\\upload";
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file")MultipartFile file, RedirectAttributes redirectAttributes){
+        if (file.isEmpty()){
+            redirectAttributes.addFlashAttribute("message"," Select File upload");
+            return "redirect:status";
+        }
+
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Path.of((UPLOADED_PATH + file.getOriginalFilename()));
+            Files.write(path,bytes);
+
+            redirectAttributes.addFlashAttribute(
+                    "message", " Success upload file" + file.getOriginalFilename());
+        }catch (IOException exception){
+            exception.printStackTrace();
+        }
+        return "redirect:status";
+    }
+    @GetMapping("/status")
+    public String uploadStatus(){
+        return "status";
+    }
+}
